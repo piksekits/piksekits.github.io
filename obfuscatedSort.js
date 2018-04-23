d9cbecde4399591afb0f600d7024d7e9c6c1bcad
piksekits <38667797+piksekits@users.noreply.github.com>
2018-04-23 23:05:13 +0300

    Create obfuscatedSort.js

---
+++ obfuscatedSort.js
@@ -0,0 +1,16 @@
+function obfuscatedSort(a)
+{
+    var b;
+    do {
+        b = "0";
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var c = a[i];
+                a[i] = a[i+1];
+                a[i+1] = c;
+                b = "1";
+            }
+        }
+    } while (b);
+	return a;
+}
