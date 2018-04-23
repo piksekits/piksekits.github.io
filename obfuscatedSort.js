f66d017292379f1754adedd6f03b6362c34697a9
piksekits <38667797+piksekits@users.noreply.github.com>
2018-04-23 23:23:44 +0300

    Update obfuscatedSort.js

--- obfuscatedSort.js
+++ obfuscatedSort.js
@@ -1,16 +1,16 @@
 function ob(a)
 {
-    var b;
-    do {
-        b = 0;
-        for (var i=0; i < a.length-1; i++) {
-            if (a[i] > a[i+1]) {
-                var c = a[i];
-                a[i] = a[i+1];
-                a[i+1] = c;
-                b = 1;
-            }
-        }
-    } while (b);
-	return a;
+var b;
+do {
+b = 0;
+for (var i=0; i < a.length-1; i++) {
+if (a[i] > a[i+1]) {
+var c = a[i];
+a[i] = a[i+1];
+a[i+1] = c;
+b = 1;
+}
+}
+} while (b);
+return a;
 }
